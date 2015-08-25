29ca1594cc9ad5a0558cf688043f8d152f0bd085
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2015-08-25 02:26:41 -0300

    new api methods and fixes

--- src/MapasSDK.php
+++ src/MapasSDK.php
@@ -2,8 +2,8 @@
 
 namespace MapasSDK;
 
-use Curl\Curl,
-    JWT;
+use Curl\Curl;
+use JWT;
 
 class MapasSDK {
 
@@ -19,47 +19,78 @@ class MapasSDK {
         $this->_algo = $algo;
     }
 
-    public function request($method, $url, array $data = [], $headers = []) {
+    public function apiRequest($method, $url, array $data = [], $headers = []) {
         $curl = new Curl;
 
-        $jwt = JWT::encode(
-            [
+        $jwt = JWT::encode([
                 'tm' => microtime(true),
                 'pk' => $this->_pubKey
             ], $this->_priKey, $this->_algo     // Algorithm used to sign the token, see https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40#section-3
         );
 
 
+        $data['isAjax'] = true;
+        
         $curl->setHeader('authorization', $jwt);
-        $curl->setHeader('X_REQUESTED_WITH', 'XMLHttpRequest');
 
         foreach ($headers as $k => $v) {
             $curl->setHeader($k, $v);
         }
 
         $curl->$method($this->_mapasInstanceUrl . $url, $data);
+        
         $curl->close();
-        return $curl->response;
+        
+        $responseObject = json_decode($curl->response);
+        
+        if(json_last_error() === JSON_ERROR_NONE){
+            $curl->response = $responseObject;
+        }
+        
+        return $curl;
     }
 
-    public function get($url, array $data = [], $headers = []) {
-        return $this->request('get', $url, $data, $headers);
+    public function apiGet($url, array $data = [], $headers = []) {
+        return $this->apiRequest('get', $url, $data, $headers);
     }
 
-    public function post($url, array $data = [], $headers = []) {
-        return $this->request('post', $url, $data, $headers);
+    public function apiPost($url, array $data = [], $headers = []) {
+        return $this->apiRequest('post', $url, $data, $headers);
     }
 
-    public function put($url, array $data = [], $headers = []) {
-        return $this->request('put', $url, $data, $headers);
+    public function apiPut($url, array $data = [], $headers = []) {
+        return $this->apiRequest('put', $url, $data, $headers);
     }
 
-    public function patch($url, array $data = [], $headers = []) {
-        return $this->request('patch', $url, $data, $headers);
+    public function apiPatch($url, array $data = [], $headers = []) {
+        return $this->apiRequest('patch', $url, $data, $headers);
     }
 
-    public function delete($url, array $data = [], $headers = []) {
-        return $this->request('delete', $url, $data, $headers);
+    public function apiDelete($url, array $data = [], $headers = []) {
+        return $this->apiRequest('delete', $url, $data, $headers);
+    }
+
+    public function getEntity($entityType, $id, $fields) {
+        return $this->apiGet("api/{$entityType}/findOne", [
+            'id' => "EQ({$id})",
+            '@select' => $fields
+        ]);
+    }
+
+    public function createEntity($entityType, array $data) {
+        return $this->apiPost("{$entityType}/index", $data);
+    }
+
+    public function updateEntity($entityType, $id, array $data) {
+        return $this->apiPut("$entityType/single/{$id}", $data);
+    }
+
+    public function patchEntity($entityType, $id, array $data) {
+        return $this->apiPatch("$entityType/single/{$id}", $data);
+    }
+
+    public function deleteEntity($entityType, $id) {
+        return $this->apiDelete("{$entityType}/single/{$id}");
     }
 
 }
--- src/test.php
+++ src/test.php
@@ -3,12 +3,16 @@
 require 'MapasSDK.php';
 require '../vendor/autoload.php';
 
-$mapas = new MapasSDK\MapasSDK('http://localhost/', 'f738a2db0b3f5d94853b9e61cd73b4162b80fd5c565ac088bea54c6995e2a358', '045c2ae5fb5488ed6687789a2f7693dc9a6e4ac1a05aed3631c0e38f329d200598ba0fcab9c14f73e5ff1d2a06d3fbeea4caf4d61c7331e74a5595d4b5c6d999');
+$mapas = new MapasSDK\MapasSDK(
+    'http://mapas.local/', 
+    '7lPnTbPby84S10gA4TDwqVuBACNBEkdF', 
+    'kFDSzQdThMpzG3x4WoVY2GDOaH1fze0H0oBBZmtquvC1sI6R32xJaSYLvZO2sPPL'
+);
 
 
-$newAgent = $mapas->post('agent/index', [
-    'type' => '2',
-    'name' => 'Fulano',
+$newAgent = $mapas->createEntity('agent', [
+    'type' => '21',
+    'name' => 'Fulano ' . date('Y/m/d H:i:s'),
     'shortDescription' => 'Oi',
     'terms' => [
         'area' => [
@@ -23,8 +27,10 @@ $newAgent = $mapas->post('agent/index', [
 ]);
 
 var_dump($newAgent);
+var_dump($newAgent->response);
 
-$agents = $mapas->get('api/agent/find/', [
+
+$agents = $mapas->apiGet('api/agent/find/', [
     '@select' => 'id,name,documento,emailPrivado',
     'user' => 'EQ(@User:1)'
 ]);
